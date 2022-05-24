27dfc54f3db9ec4871b41cc709abe376ce9ee31b
Daniel Braaksma <danebraaksma@hotmail.com>
2022-05-23 22:27:10 -0400

    added state and onchange func to App

--- src/App.js
+++ src/App.js
@@ -1,11 +1,21 @@
 import React from "react"
 
 function App() {
+    const [text, setText] = useState("")
+
+    function handleChange(e) {
+        const {value} = e.target //destructure from the event object
+        setText(value)  //set state with the new value of input area
+    }
+
     return (
         <div>
             <h1>How fast do you type?</h1>
-            <textarea />
-            <h4>Time reminaing: ???</h4>
+            <textarea
+                onChange={handleChange} // update the textarea on every keystroke
+                value={text}
+            />
+            <h4>Time remaining: ???</h4>
             <button>Start</button>
             <h1>Word count: ???</h1>
         </div>
