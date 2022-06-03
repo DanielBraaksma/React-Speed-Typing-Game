53ac1815856c5dded69e5a6fc533f669e55c4cc8
Daniel Braaksma <danebraaksma@hotmail.com>
2022-06-02 22:02:19 -0400

    refactor to use custom hook

--- src/App.js
+++ src/App.js
@@ -1,44 +1,16 @@
-import React from "react"
+import React, {useState, useEffect, useRef} from "react"
+import useWordGame from "./useWordGame"
 
 function App() {
-    const STARTING_TIME = 5
-    const [text, setText] = React.useState("")
-    const [timeRemaining, setTimeRemaining] = React.useStatee(STARTING_TIME)
-    const [isTimeRunning, setIsTimeRunning] = React.useState(false)
-    const textBoxRef = useRef(null)
-
-    function handleChange(e) {
-        const {value} = e.target //destructure from the event object
-        setText(value)  //set state with the new value of input area
-    }
-
-    function calculateWordCount(text) {
-        const wordsArr = text.trim().split(" ")
-        return wordsArr.filter(word => word !== "").length
-    }
-
-    function startGame() {
-        setIsTimeRunning(true)
-        setTimeRemaining(STARTING_TIME)
-        setText("")
-        textBoxRef.current.disabled = false
-        textBoxRef.current.focus()
-    }
-
-    function endGame() {
-        setIsTimeRunning(false)
-        setWordCount(calculateWordCount(text))
-    }
-
-    React.useEffect(() => {
-        if(isTimeRunning && timeRemaining > 0) {
-            setTimeout(() => {
-                setTimeRemaining(time => time - 1)
-            }, 1000)
-        } else if(timeRemaining === 0) {
-            endGame()
-        }
-    }, [timeRemaining, isTimeRunning])
+    const {
+        textBoxRef,
+        handleChange,
+        text,
+        isTimeRunning,
+        timeRemaining,
+        startGame,
+        wordCount
+    } = useWordGame(10)
 
     return (
         <div>
